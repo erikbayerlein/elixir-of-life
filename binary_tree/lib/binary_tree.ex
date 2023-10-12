2e6176dfe7da350cd049e919b3f7e372c9aa6ae6
erikbayerlein <erik.bayer@icloud.com>
2023-10-12 10:59:39 -0300

    Initial commit

---
+++ binary_tree/lib/binary_tree.ex
@@ -0,0 +1,18 @@
+defmodule BinaryTree do
+  @moduledoc """
+  Documentation for `BinaryTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BinaryTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
