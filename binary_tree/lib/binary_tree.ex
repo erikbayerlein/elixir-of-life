ddbf6c02cd140437eb5dd7a4bc6a42f6b484f7d3
erikbayerlein <erik.bayer@icloud.com>
2023-10-12 16:25:09 -0300

    parser

--- binary_tree/lib/binary_tree.ex
+++ binary_tree/lib/binary_tree.ex
@@ -1,18 +1,3 @@
 defmodule BinaryTree do
-  @moduledoc """
-  Documentation for `BinaryTree`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BinaryTree.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
